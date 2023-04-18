b3bf25f1e208e2e518047e36ba74574f5ee36d8e
adaowap <adao.filho@mercadolivre.com>
2023-04-18 13:07:13 -0300

    Inclusão de novos atributos e anotação @Builder

--- src/main/java/br/com/meli/anuncios/entities/Anuncio.java
+++ src/main/java/br/com/meli/anuncios/entities/Anuncio.java
@@ -4,15 +4,26 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Anuncio {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String descricao;
+    private double precoNormal;
+    private double precoPromo;
+
+
 
     //private List<Visualizacao> visualizacoes;
 }

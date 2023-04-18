088eae08ff5abcd327ee42db4f13232fd7f755d8
adaowap <adao.filho@mercadolivre.com>
2023-04-18 13:11:18 -0300

    Ciração de Dto de saída e método de converção de model anucio para DtoAnuncioOut

--- src/main/java/br/com/meli/anuncios/dto/AnuncioDtoOut.java
+++ src/main/java/br/com/meli/anuncios/dto/AnuncioDtoOut.java
@@ -1,18 +1,33 @@
 package br.com.meli.anuncios.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import br.com.meli.anuncios.entities.Anuncio;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AnuncioDtoOut {
 
     private Long id;
-
     private String name;
+    private String descricao;
+    private double precoNormal;
+    private double precoPromo;
+
+    public static AnuncioDtoOut fromModel(Anuncio anuncio){
+        if (anuncio == null){
+            return null;
+        }
+
+        return AnuncioDtoOut.builder()
+                .id(anuncio.getId())
+                .name(anuncio.getName())
+                .descricao(anuncio.getDescricao())
+                .precoNormal(anuncio.getPrecoNormal())
+                .precoPromo(anuncio.getPrecoPromo())
+                .build();
+    }
 
 }

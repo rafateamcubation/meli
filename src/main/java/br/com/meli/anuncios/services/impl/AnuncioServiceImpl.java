f5b25bdd4d08b5b1327564c31f00406e214cd6a7
adaowap <adao.filho@mercadolivre.com>
2023-04-18 13:16:35 -0300

    Refatoração de métodos de AnuncioServiceImpl e otimização da lógica de validação e excessões

--- src/main/java/br/com/meli/anuncios/services/impl/AnuncioServiceImpl.java
+++ src/main/java/br/com/meli/anuncios/services/impl/AnuncioServiceImpl.java
@@ -1,15 +1,18 @@
 package br.com.meli.anuncios.services.impl;
 
-import br.com.meli.anuncios.dto.AnuncioDtoIn;
+import br.com.meli.anuncios.dto.AnuncioDto;
 import br.com.meli.anuncios.dto.AnuncioDtoOut;
 import br.com.meli.anuncios.entities.Anuncio;
+import br.com.meli.anuncios.exceptions.BlankNameException;
 import br.com.meli.anuncios.exceptions.ResourceNotFoundException;
+import br.com.meli.anuncios.exceptions.ValuesNotValidException;
 import br.com.meli.anuncios.repositories.AnuncioRepository;
 import br.com.meli.anuncios.services.AnuncioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,51 +21,78 @@ public class AnuncioServiceImpl implements AnuncioService {
     private AnuncioRepository repository;
 
     @Override
-    public AnuncioDtoOut create(AnuncioDtoIn anuncioDtoIn) {
-        Anuncio entity = new Anuncio();
-        entity.setName(anuncioDtoIn.getName());
-
-        Anuncio savedEntity = repository.save(entity);
-
-        AnuncioDtoOut returnDto = new AnuncioDtoOut();
-        returnDto.setId(savedEntity.getId());
-        returnDto.setName(savedEntity.getName());
-
-        return returnDto;
-    }
-
-    @Override
     public AnuncioDtoOut findById(Long id) {
         Anuncio entity = repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Anuncio nao encontrado"));
 
-        AnuncioDtoOut returnDto = new AnuncioDtoOut(entity.getId(), entity.getName());
-
-        return returnDto;
+        return AnuncioDtoOut.fromModel(entity);
     }
 
     @Override
     public List<AnuncioDtoOut> findAll() {
         List<Anuncio> entities = repository.findAll();
 
-        return entities.stream().map(e -> new AnuncioDtoOut(e.getId(), e.getName())).collect(Collectors.toList());
+        if (entities.isEmpty()){
+            throw new ResourceNotFoundException("Não existem anúncios!");
+        } else {
+            return entities.stream().map(e -> AnuncioDtoOut.fromModel(e))
+                    .collect(Collectors
+                            .toList());
+        }
     }
 
     @Override
-    public AnuncioDtoOut update(AnuncioDtoOut anuncioDtoOut) {
-        Anuncio entity = repository.findById(anuncioDtoOut.getId())
-                .orElseThrow(() -> new ResourceNotFoundException("Anuncio nao encontrado"));
+    public AnuncioDtoOut create(AnuncioDto anuncioDto) {
 
-        entity.setName(anuncioDtoOut.getName());
+        checks(anuncioDto);
 
-        entity = repository.save(entity);
+        anuncioDto.setId(null);
 
-        return new AnuncioDtoOut(entity.getId(), entity.getName());
+        Anuncio anuncio = anuncioDto.toModel();
+        repository.save(anuncio);
+
+        return AnuncioDtoOut.fromModel(anuncio);
+    }
+
+    @Override
+    public AnuncioDtoOut update(AnuncioDto anuncioDto) {
+        checks(anuncioDto);
+
+        Anuncio anuncio =  anuncioDto.toModel();
+
+                repository.findById(anuncio.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Anuncio não encontrado"));
+
+
+        repository.save(anuncio);
+
+        return AnuncioDtoOut.fromModel(anuncio);
     }
 
     @Override
     public void deleteById(Long id) {
-        findById(id);
-        repository.deleteById(id);
+        Anuncio entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Anúcio não encontrado!"));
+
+        repository.save(entity);
+    }
+
+    private void checks(AnuncioDto anuncioDto){
+        checkIsBlank(anuncioDto.getName());
+        checkIsBlank(anuncioDto.getDescricao());
+        checkIsValueNotValid(anuncioDto.getPrecoNormal(), anuncioDto.getPrecoPromo());
+    }
+
+    private void checkIsBlank(String value) {
+        if (Objects.isNull(value) || value.isEmpty()) {
+            throw new BlankNameException("Valores de entrada não podem ser nullos ou vázios!");
         }
+    }
+
+    private void checkIsValueNotValid(double precoNormal, double precoPromo){
+        if (precoPromo >= precoNormal){
+            throw new ValuesNotValidException("Valores de entradas inválidos!");
+        }
+    }
+
 }
